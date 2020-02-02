ea4c77cc846b4f68bd73389c787141acc0da7eba
Alexander Gusev <san4es021@gmail.com>
2020-02-02 20:33:59 +0300

    Sounds Added

--- src/components/RouletteComponent.vue
+++ src/components/RouletteComponent.vue
@@ -2,11 +2,14 @@
   <div>
     <div class="form">
       <p>Время: <input type="text" v-model="time" /></p>
-      <p>Номер элемента: <input type="text" v-model="itemNum" /></p>
-      <button @click="spin()">Запустить</button>
+      <p>
+        Номер элемента: <input type="text" v-model="itemNum" /> (от 4 до 50)
+      </p>
+      <button @click="spin()" :disabled="spinActivated">Запустить</button>
       <button @click="reset()">Сбросить</button>
     </div>
     <div class="roulette" ref="rouletteWrapper">
+      <div class="roulette-cursor" ref="cursor"></div>
       <div class="roulette-wrapper" ref="roulette">
         <div
           class="roulette-item"
@@ -21,13 +24,21 @@
 </template>
 
 <script>
+import RouletteSound from "@/assets/roulette_spin.mp3";
+
 export default {
   data: () => ({
     time: 8000,
-    itemNum: 45
+    itemNum: 45,
+    itemToStartDetecting: 4,
+    spinActivated: false
   }),
+  mounted() {
+    this.detectCursor();
+  },
   methods: {
     spin() {
+      this.spinActivated = true;
       let { rouletteWrapper } = this.$refs;
       let { roulette } = this.$refs;
       let item = this.$refs.rouletteItem[this.itemNum - 1];
@@ -46,12 +57,33 @@ export default {
       roulette.style.transform = `translateX(${-translateX}px)`;
     },
     reset() {
+      this.spinActivated = false;
       let { roulette } = this.$refs;
       this.time = 8000;
       this.itemNum = 45;
+      this.itemToStartDetecting = 4;
 
       roulette.style.transition = `all 0ms cubic-bezier(0.32, 0.64, 0.45, 1)`;
       roulette.style.transform = `translateX(0px)`;
+    },
+    detectCursor() {
+      if (this.$refs.rouletteItem[this.itemToStartDetecting - 1]) {
+        let { cursor } = this.$refs;
+        let cursorRect = cursor.getBoundingClientRect();
+
+        let item = this.$refs.rouletteItem[this.itemToStartDetecting - 1];
+        let itemRect = item.getBoundingClientRect();
+
+        let entered = cursorRect.left > itemRect.left;
+        let left = cursorRect.left > itemRect.left + itemRect.width;
+
+        if (entered && !left) {
+          new Audio(RouletteSound).play();
+          this.itemToStartDetecting = this.itemToStartDetecting + 1;
+        }
+      }
+
+      window.requestAnimationFrame(this.detectCursor);
     }
   }
 };
@@ -65,7 +97,7 @@ export default {
   border: 1px solid #ccc
   height: 200px
   position: relative
-  &:after
+  &-cursor
     content: ""
     position: absolute
     height: 100%
@@ -74,11 +106,13 @@ export default {
     opacity: .5
     left: calc(50% - 1px)
     top: 0
+    z-index: 10
   &-wrapper
     width: 99999px
     height: 200px
     display: flex
     align-items: center
+    transform: translateX(0px)
   &-item
     height: 120px
     width: 120px
